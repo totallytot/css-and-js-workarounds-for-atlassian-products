911e1155f3581605108b4edabf4f3940671af1e7
Totally Tot <fcukthepop@gmail.com>
2018-11-13 19:58:33 +0300

    Update change-field-desc-based-on-select-list-value.js

--- jira/change-field-desc-based-on-select-list-value.js
+++ jira/change-field-desc-based-on-select-list-value.js
@@ -1,6 +1,5 @@
-var defaultDesc;
+var defaultDesc = "Select value in order to see description.";
 AJS.toInit(function () {
-	defaultDesc = AJS.$("#customfield_10938").next().text();
     changeFieldDesc();
 });
 AJS.$("#customfield_10938").change(function () {
@@ -10,7 +9,6 @@ function changeFieldDesc(){
 	var messageA = "Security patch to a third-party component\n\Replacement of an infrastructure element (router, firmware) determined to present no risk and have no functional impact\n\Manual correction of an erroneous data field.";
 	var messageB = "Change to database values or configuration file without impact on documented functionality (defect correction).";
 	var messageC = "Any code change / deployment of a new software executable\n\Any change that could potentially affect the safety, performance or functionality of a software device";
-	 
 	if (AJS.$("#customfield_10938").val() == 10632) AJS.$("#customfield_10938").next().text(messageA);
 	else if (AJS.$("#customfield_10938").val() == 10633) AJS.$("#customfield_10938").next().text(messageB);
 	else if (AJS.$("#customfield_10938").val() == 10634) AJS.$("#customfield_10938").next().text(messageC);
