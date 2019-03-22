a6f7f5ee1dd51903fc359597b45c3b2d4fdb7c15
Totally Tot <fcukthepop@gmail.com>
2019-03-22 19:45:05 +0300

    Update hide-delete-project-button-from-non-system-admins.js

--- jira/hide-delete-project-button-from-non-system-admins.js
+++ jira/hide-delete-project-button-from-non-system-admins.js
@@ -1,13 +1,17 @@
-AJS.toInit(function () {
-  AJS.$("#view_delete_project").hide();
-  AJS.$(".delete-project").hide();
-  var userName = AJS.params.loggedInUser;
-  AJS.$.get("/rest/api/latest/user", {username: userName, expand: "groups"}, function (data) {
-	  var groups = data.groups;
-	  var userGroups = jQuery.map(groups.items, function (val, j) { return val.name });
-	  if (jQuery.inArray("jira-system-administrators", userGroups) > -1) {
-	    AJS.$("#view_delete_project").show();
-	    AJS.$(".delete-project").show();
-    }
-  });
-});
+AJS.toInit(function () {	
+ if (window.location.href.endsWith("/secure/project/ViewProjects.jspa") || window.location.href.search("plugins/servlet/project-config/") > -1) {
+   AJS.$("#view_delete_project").hide();
+   AJS.$(".delete-project").hide();    
+   AJS.$.get("/rest/auth/1/session", function (data) {
+     userName = data.name
+	 AJS.$.get("/rest/api/latest/user", {username: userName, expand: "groups"}, function (data) {
+	   var groups = data.groups;
+	   var userGroups = jQuery.map(groups.items, function (val, j) { return val.name });
+	   if (jQuery.inArray("jira-system-administrators", userGroups) > -1) {
+	     AJS.$("#view_delete_project").show();
+	     AJS.$(".delete-project").show();
+       } 
+     });
+   });
+ }
+}); 
