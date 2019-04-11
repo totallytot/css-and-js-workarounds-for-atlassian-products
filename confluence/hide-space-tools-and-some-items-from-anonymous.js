87403a4ff16de0cdb21d5eae6a526a28e72870d3
Totally Tot <fcukthepop@gmail.com>
2019-04-12 00:11:11 +0300

    Update hide-space-tools-and-some-items-from-anonymous.js

--- confluence/hide-space-tools-and-some-items-from-anonymous.js
+++ confluence/hide-space-tools-and-some-items-from-anonymous.js
@@ -3,6 +3,7 @@
 AJS.toInit(function(){ 
   if (AJS.params.remoteUserKey === '') {
     AJS.$('#space-tools-menu-trigger').hide();
+    AJS.$('.ia-scrollable-section').css('margin-bottom', '-40px');
     AJS.$('#view-page-info-link').hide();
     AJS.$('#action-view-history-link').hide(); 
     AJS.$('#view-in-hierarchy-link').hide(); 
