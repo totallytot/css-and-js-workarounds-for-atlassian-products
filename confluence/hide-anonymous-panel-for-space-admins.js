f31d1ec49b7ed9727134c7587e5eda1f0694fc67
Totally Tot <fcukthepop@gmail.com>
2019-09-04 11:30:40 +0300

    Minor fix

--- confluence/hide-anonymous-panel-for-space-admins.js
+++ confluence/hide-anonymous-panel-for-space-admins.js
@@ -2,8 +2,10 @@
 Problem: https://jira.atlassian.com/browse/CONFSERVER-44116
 Environment: Confluence 5.10.9 
 */
+<script>
 AJS.toInit(function(){
    if (!AJS.params.isConfluenceAdmin) {
-      AJS.$('#aPermissionsTable, .steptitle, .stepdesc, .buttons-container:eq(2)').hide();
+      AJS.$('#aPermissionsTable').hide();
    }
 });
+</script>
