58d236398dba17ddf921cef99335033eb9004951
TotallyTot <fcukthepop@gmail.com>
2019-10-31 06:48:21 +0300

    js snippet fot button creation added

---
+++ jira/inesert-button-for-sub-task-creation.js
@@ -0,0 +1,18 @@
+<script>
+function insertSubtaskCreationBtn() {
+    let projectKey = JIRA.API.Projects.getCurrentProjectKey();
+    if (projectKey === "TNH") {
+        let createSubtaskBtn = AJS.$('#opsbar-opsbar-admin').append("<button class='aui-button'>Create sub-task</button>");
+        createSubtaskBtn.click(function() {
+            let issueId = JIRA.Issue.getIssueId();
+            JIRA.Forms.createSubtaskForm({parentIssueId: issueId}).asDialog({windowTitle: 'Create Subtask'}).show();
+        });
+    }
+}
+AJS.toInit(function () {
+    insertSubtaskCreationBtn();
+});
+AJS.$(document).bind(JIRA.Events.ISSUE_REFRESHED, function() {
+    insertSubtaskCreationBtn();
+});
+</script>
