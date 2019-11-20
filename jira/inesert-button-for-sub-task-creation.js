7f2a4036d2c69bad4b4e2aeec808c84dd3a8f0ca
Totally Tot <fcukthepop@gmail.com>
2019-11-20 12:35:31 +0300

    minor fix for sub-task btn

--- jira/inesert-button-for-sub-task-creation.js
+++ jira/inesert-button-for-sub-task-creation.js
@@ -3,7 +3,8 @@
 function insertSubtaskCreationBtn() {
     let projectKey = JIRA.API.Projects.getCurrentProjectKey();
     if (projectKey === "PCS") {
-        let createSubtaskBtn = AJS.$('#opsbar-opsbar-admin').append("<button class='aui-button'>Create sub-task</button>");
+        AJS.$('#opsbar-opsbar-admin').append("<button class='aui-button' id='custom-sb-btn'>Create sub-task</button>");
+        let createSubtaskBtn = AJS.$('#custom-sb-btn');
         createSubtaskBtn.click(function() {
             let issueId = JIRA.Issue.getIssueId();
             JIRA.Forms.createSubtaskForm({parentIssueId: issueId}).asDialog({windowTitle: 'Create Subtask'}).show();
