83fe439abef3a6eadc5b919b75b26dd1ecec2fde
Totally Tot <fcukthepop@gmail.com>
2020-03-20 15:46:59 +0300

    refactoring

--- jira/hide-section-based-on-users-group.js
+++ jira/hide-section-based-on-users-group.js
@@ -1,32 +1,38 @@
-function hideitems() {
-    var user;
-AJS.$.ajax({
-	url: "/rest/gadget/1.0/currentUser",
-	type: 'get',
-	dataType: 'json',
-	async: false,
-	success: function(data) { user = data.username; } });
-var groups;
-AJS.$.ajax({
-	url: "/rest/api/latest/user?username="+user+"&expand=groups",
-	type: 'get',
-	dataType: 'json',
-	async: false,
-	success: function(data) { groups = data.groups.items; }});
-var group ;
-group = "SchaefferPoeschlClient";
-for (i = 0; i < groups.length; i++){
-console.log(groups[i].name);
-	if (groups[i].name==group){ 
-		AJS.$("#jeti-trigger").hide(); //hide email button
+AJS.toInit(function() {
+    if (window.location.href.includes("/browse/")) {
+        var affectedGroup = "SchaefferPoeschlClient";
+        var affectedUser = AJS.params.loggedInUser;
+        hideItemsForUserInCertainGroup(affectedUser, affectedGroup);
+
+        AJS.$(document).bind('dialogContentReady', function() { 
+            hideItemsForUserInCertainGroup(affectedUser, affectedGroup); 
+        });
+        
+        JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function () { 
+            hideItemsForUserInCertainGroup(affectedUser, affectedGroup); 
+        });
+    }
+});
+
+function hideItemsForUserInCertainGroup(user, group) {
+    let userGroups = getUserGroups(user);
+    let searchResult = userGroups.find(element => element.name == group);
+    if (searchResult != null) {
+        AJS.$("#jeti-trigger").hide(); //hide email button
         AJS.$("#wrap-labels").hide(); //hide labels
         AJS.$("#assignee-val").parents("dl").hide(); //hide assignee
-		AJS.$("#resolution-val").parents("li").hide(); //hide resolution
-		
-        }
-	}
-};
-AJS.$(document).toInit(function(){hideitems()});
-JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context) {
-    hideitems();
-});
+        AJS.$("#resolution-val").parents("li").hide(); //hide resolution
+    }
+}
+
+function getUserGroups(user) {
+    let groups = null;
+    AJS.$.ajax({
+        url: "/rest/api/latest/user?username="+user+"&expand=groups",
+        type: 'get',
+        dataType: 'json',
+        async: false,
+        success: function(data) { groups = data.groups.items; }
+    });
+    return groups;
+}
