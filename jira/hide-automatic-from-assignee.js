895ba3abc41f192e95d2f01c9059d6074817d51e
Totally Tot <fcukthepop@gmail.com>
2020-05-01 01:08:03 +0300

    Update hide-automatic-from-assignee.js

--- jira/hide-automatic-from-assignee.js
+++ jira/hide-automatic-from-assignee.js
@@ -1,8 +1,11 @@
+// Working with the NEW_CONTENT_ADDED event: https://developer.atlassian.com/server/jira/platform/extending-inline-edit-for-jira-plugins/
 AJS.toInit(function() {
     AJS.$("#assignee-field").val("");
-    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function() {
-        setTimeout(function() { 
-            AJS.$("#assignee-field").val(""); 
-        }, 50);
+    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function(e, context, reason) {
+        if (reason == JIRA.CONTENT_ADDED_REASON.dialogReady) {
+            setTimeout(function() { 
+                AJS.$("#assignee-field").val(""); 
+            }, 50);
+        }
     });
 });
