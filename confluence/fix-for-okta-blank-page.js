0228b9ec40200a0ec6a50e8b577466d44c780fe2
TotallyTot <fcukthepop@gmail.com>
2022-03-06 12:01:14 +0300

    Create fix-for-okta-blank-page.js

---
+++ confluence/fix-for-okta-blank-page.js
@@ -0,0 +1,6 @@
+<!-- fix for blank page - okta issue -->
+<script>
+if (window.location.href.endsWith("/index.action")) {
+    window.location.href = "https://confluence-migration.example.net/#all-updates";
+}
+</script>
